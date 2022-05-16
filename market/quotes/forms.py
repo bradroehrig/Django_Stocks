0e5ec0bfd6698f9f30e13ee39e7cfc7ce3a7701f
Brad Roehrig'git config --global user.email bradroehrig@yahoo.comgit config --global --listgit config --global user.name Brad <63978952+bradroehrig@users.noreply.github.com>
2022-05-16 11:48:33 -0500

    market2

--- market/quotes/forms.py
+++ market/quotes/forms.py
@@ -1,18 +1,4 @@
 from django import forms  
-from quotes.models import List
-from.models import Stock
-
-class ListForm(forms.ModelForm):
-    class Meta:
-        model = List
-        fields = "__all__"
-        
-    def clean_field(self):
-        item = self.cleaned_data['item']
-        if len(item) < 3:
-            raise forms.ValidationError("Need at least three characters")
-        return item
-
 from.models import Stock
 
 class StockForm(forms.ModelForm):
--- market/quotes/urls.py
+++ market/quotes/urls.py
@@ -4,20 +4,8 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('home', views.home, name='home'),
     path('base', views.base, name='base'),
-    path('list', views.list, name='list'),  
-    path('show_list',views.show_list, name='show_list'),  
-    path('edit_list/<int:id>', views.edit_list, name='edit_list'),
-    path('edit_list/home', views.show_list, name='show_list'),    
-    path('update_list/<int:id>', views.update_list, name="update_list"),
-    path('delete_list/<int:id>', views.destroy_list, name='delete_list'),
-    # path('', views.home, name="home"),
     path('about.html', views.about, name="about"),
     path('add_stock.html', views.add_stock, name="add_stock"),
     path('delete/<stock_id>', views.delete, name='delete'),
     path('delete_stock.html', views.delete_stock, name="delete_stock"),
-    
-    
-    
-    
-    
 ]
